b478b239bb226e9b29148d7454b411c7e790d03d
Felipe Freitas dos Santos Silva <82713076+felipesansi@users.noreply.github.com>
2024-06-07 15:46:32 -0300

    Mehorada pagina

--- gerador.js
+++ gerador.js
@@ -1,17 +1,24 @@
+function GeradorSenha() {
+    let comprimento = document.getElementById('tamanho').value;
+    if(comprimento===""){
+        alert("Coloque um número ")
+    }else{
+   
+    let caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_0123456789.';
+    let senha = '';
 
-function GeradorSenha(){
- let comprimento =document.getElementById('tamanho').value;
-let contem = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_0123456789.'
-let senha = '';
-for( let i = 0; i < comprimento; i++){
+    for (let i = 0; i < comprimento; i++) {
+        let aleatorio_index = Math.floor(Math.random() * caracteres.length);
+        senha += caracteres[aleatorio_index];
+    }
 
-let aletorio_index = Math.floor(Math.random() * contem.length)
-
-senha += contem[aletorio_index];
-
-
-document.getElementById("gerada_senha").innerHTML =senha;
+    document.getElementById("gerada_senha").value = senha;
 }
-
 }
-
+ function CopiarSenha(){
+   let senha_gerada =document.getElementById("gerada_senha");
+   senha_gerada.select();
+   senha_gerada.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+   alert("Senha copiada: " + senha_gerada.value);
+ }
